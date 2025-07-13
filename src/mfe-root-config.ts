ae2c087282c77694fb346927322a92b6952f6b9e
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-07-13 12:42:33 -0300

    feat: Register navbar route

--- src/mfe-root-config.ts
+++ src/mfe-root-config.ts
@@ -1,11 +1,20 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
+function shouldBeActive(location) {
+  return !location.pathname.startsWith("/users");
+}
+
 registerApplication({
   name: "@mfe/users-route",
   app: () => System.import<LifeCycles>("@mfe/users-route"),
   activeWhen: ["/users"],
 });
 
+registerApplication({
+  name: "@mfe/navbar-route",
+  app: () => System.import<LifeCycles>("@mfe/navbar-route"),
+  activeWhen: shouldBeActive,
+});
 registerApplication({
   name: "@mfe/home-route",
   app: () => System.import<LifeCycles>("@mfe/home-route"),
